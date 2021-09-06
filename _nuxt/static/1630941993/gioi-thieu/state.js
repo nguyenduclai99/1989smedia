e9edf25105ab271565ff28b3cc91125d0fcbf59a
nguyenduclai <laind@ttc-solutions.com.vn>
2021-09-06 22:27:29 +0700

    test deploy

---
+++ _nuxt/static/1630941993/gioi-thieu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"1989smedia\u002F_nuxt\u002Fstatic\u002F1630941993",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgioi-thieu",config:{_app:{basePath:"1989smedia\u002F",assetsPath:"1989smedia\u002F_nuxt\u002F",cdnURL:a}}}}(null));
