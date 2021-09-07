716a7aaf776f3bb2fc0890bdc2fdbd1d3190a228
nguyenduclai <laind@ttc-solutions.com.vn>
2021-09-07 08:23:07 +0700

    test deploy

--- _nuxt/static/1630941993/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"1989smedia\u002F_nuxt\u002Fstatic\u002F1630941993",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"1989smedia\u002F",assetsPath:"1989smedia\u002F_nuxt\u002F",cdnURL:a}}}}(null));
