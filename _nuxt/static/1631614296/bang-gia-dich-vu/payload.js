41cd09d173c636104f807806160c29c9cecb325b
nguyenduclai <nguyen.duc.lai@sun-asterisk.com>
2022-02-05 21:55:43 +0700

    update

--- _nuxt/static/1631614296/bang-gia-dich-vu/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/bang-gia-dich-vu", {data:[{}],fetch:{},mutations:[]});
--- _nuxt/static/1631614296/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fbang-gia-dich-vu","\u002Fdang-ky","\u002Fgioi-thieu","\u002Fdang-nhap","\u002Flien-he"]})
